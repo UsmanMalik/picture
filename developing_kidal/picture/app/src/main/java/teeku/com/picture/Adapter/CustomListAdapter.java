0ebfd4917789edf9248ba33d68595fb9136c13db
UsmanMalik <malik.usman.ek@gmail.com>
2015-11-30 12:20:14 +0500

    graphics added, content to be added.

--- developing_kidal/picture/app/src/main/java/teeku/com/picture/Adapter/CustomListAdapter.java
+++ developing_kidal/picture/app/src/main/java/teeku/com/picture/Adapter/CustomListAdapter.java
@@ -4,6 +4,7 @@ package teeku.com.picture.Adapter;
  * Created by usman on 11/22/15.
  */
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -25,11 +26,29 @@ public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.Vi
     private List<Item> countries;
     private int rowLayout;
     private Context mContext;
+    private int colors[];
+
 
     public CustomListAdapter(List<Item> countries, int rowLayout, Context context) {
         this.countries = countries;
         this.rowLayout = rowLayout;
         this.mContext = context;
+        setColorsArray();
+    }
+
+    public void setColorsArray(){
+
+
+        // Set colors array
+        TypedArray ta = mContext.getResources().obtainTypedArray(R.array.colors);
+        ta.length();
+
+        colors = new int[ta.length()];
+
+        for (int j = 0; j < ta.length(); j++) {
+            colors[j] = ta.getColor(j, 0);
+        }
+        ta.recycle();
     }
 
     @Override
@@ -40,13 +59,24 @@ public class CustomListAdapter extends RecyclerView.Adapter<CustomListAdapter.Vi
 
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int i) {
+
+
+
+
+
+
+
         Item country = countries.get(i);
-        int[] colors = new int[] { R.color.abc_background_cache_hint_selector_material_dark, R.color.bright_foreground_inverse_material_dark};
+//        int[] colors_ = new int[] { R.color.abc_background_cache_hint_selector_material_dark, R.color.bright_foreground_inverse_material_dark};
         int colorPos = i % colors.length;
-        Log.e("Color change #######: " , i +"");
+        Log.e("Color change #######: ", i + "");
+
+
         viewHolder.title.setText(country.getTitle());
         viewHolder.description.setText(country.getDescription());
         viewHolder.image_path.setBackgroundColor(colors[colorPos]);
+       // ta.recycle();
+
         //viewHolder.countryImage.setImageDrawable(mContext.getDrawable(country.getImageResourceId(mContext)));
     }
 
--- developing_kidal/picture/app/src/main/java/teeku/com/picture/Adapter/CustomListChildAdapter.java
+++ developing_kidal/picture/app/src/main/java/teeku/com/picture/Adapter/CustomListChildAdapter.java
@@ -1,10 +1,12 @@
 package teeku.com.picture.Adapter;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -20,11 +22,30 @@ public class CustomListChildAdapter extends RecyclerView.Adapter<CustomListChild
     private List<ItemChild> countries;
     private int rowLayout;
     private Context mContext;
+    private int colors[];
 
     public CustomListChildAdapter(List<ItemChild> countries, int rowLayout, Context context) {
         this.countries = countries;
         this.rowLayout = rowLayout;
         this.mContext = context;
+        setColorsArray();
+
+
+    }
+
+    public void setColorsArray(){
+
+
+        // Set colors array
+        TypedArray ta = mContext.getResources().obtainTypedArray(R.array.colors);
+        ta.length();
+
+        colors = new int[ta.length()];
+
+        for (int j = 0; j < ta.length(); j++) {
+            colors[j] = ta.getColor(j, 0);
+        }
+        ta.recycle();
     }
 
     @Override
@@ -38,6 +59,9 @@ public class CustomListChildAdapter extends RecyclerView.Adapter<CustomListChild
         ItemChild country = countries.get(i);
         viewHolder.title.setText(country.getTitle());
         viewHolder.description.setText(country.getDescription());
+        int colorPos = i % colors.length;
+        viewHolder.video_path.setBackgroundColor(colors[colorPos]);
+
         //viewHolder.countryImage.setImageDrawable(mContext.getDrawable(country.getImageResourceId(mContext)));
     }
 
@@ -49,12 +73,14 @@ public class CustomListChildAdapter extends RecyclerView.Adapter<CustomListChild
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
         public TextView description;
+        public ImageView video_path;
         // public ImageView countryImage;
 
         public ViewHolder(View itemView) {
             super(itemView);
             title = (TextView) itemView.findViewById(R.id.video_title);
             description = (TextView) itemView.findViewById(R.id.video_description);
+            video_path = (ImageView) itemView.findViewById(R.id.video_path);
             //   countryImage = (ImageView)itemView.findViewById(R.id.countryImage);
         }
 
